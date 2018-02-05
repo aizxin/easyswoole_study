59acccc0f05d5103542bbb5d3952bf4a372e7f04
aizxin <aizxin@gmail.com>
2018-02-05 17:16:42 +0800

    easy-swoole

---
+++ Application/HttpController/Hello.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\HttpController;
+
+use EasySwoole\Core\Http\AbstractInterface\Controller;
+
+class Hello extends Controller
+{
+    function index()
+    {
+        $this->response()->write('Hello easySwoole!');
+    }
+
+    function article()
+    {
+        $this->response()->write('this is article');
+    }
+}
---
+++ Application/HttpController/Index.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\HttpController;
+
+use EasySwoole\Core\Http\AbstractInterface\Controller;
+
+class Index extends Controller
+{
+    function index()
+    {
+        $this->response()->write('Hello easySwoole!');
+    }
+}
